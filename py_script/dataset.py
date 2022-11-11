6c87ae978e310052c1e32ef359c73104cd1f4ac1
H. Jin <jinh@anl.gov>
2022-11-11 13:50:26 -0600

    An example for AC network

--- py_script/dataset.py
+++ py_script/dataset.py
@@ -3,10 +3,11 @@ import os.path as osp
 from typing import Callable, Optional
 
 import numpy as np
+import pandas as pd
 import torch
 from pandapower.converter import from_mpc
 from pandapower.topology import create_nxgraph
-from torch_geometric.data import Data, InMemoryDataset, HeteroData
+from torch_geometric.data import Data, HeteroData, InMemoryDataset
 
 from py_script.utils import read_file
 
@@ -51,42 +52,27 @@ class GMD(InMemoryDataset):
 
     def process(self):
         """ Process the raw file, and save to processed files. """
-
-        # TODO: process the file either using pandapower or from raw data file.
-
         # filename
         fn = self.root + "/" + self.name + ".m"
-        # NOTE: Deprecated. Switch from pandapower to raw data processing.
-        #       pandapower is not accurate
-        # net = from_mpc(fn)
-        # g = create_nxgraph(net)
-        # x = torch.tensor(net.bus[['name', 'vn_kv', 'zone', 'in_service', 'min_vm_pu',
-        #                  'max_vm_pu']].astype(float).to_numpy(), dtype=torch.float32)
-
-        # edges = np.array(list(g.edges()))
-        # edge_index = torch.tensor(edges).T
-        # print(f"# of nodes {len(g.nodes())}",
-        #       f"# of edges {len(g.edges())}")
-
-        # NOTE: process from raw data
         mpc = read_file(fn)
-        x = torch.tensor(mpc['bus'].iloc[:, 1:].to_numpy(), dtype=torch.float32)
 
-        # shift the index
-        edges = mpc['branch'].iloc[:, :2].to_numpy() - 1
-        edge_index = torch.tensor(edges).T
+        bus_gen = pd.merge(mpc['bus'], mpc['gen'], how="left", left_on="bus_i", right_on="bus")
+        # bus has 4 types
+        bus_gen = pd.concat(
+            [pd.DataFrame(np.eye(4)[bus_gen.type.to_numpy(dtype=int)]).add_prefix("t"), bus_gen], axis=1)
+        bus_gen = bus_gen.drop(['bus_i', 'bus', 'type'], axis=1)
+        x = torch.tensor(np.nan_to_num(bus_gen.to_numpy(), nan=-1), dtype=torch.float32)
+        # process edge index and features
+        edges = mpc['branch'].iloc[:, :2].to_numpy()
+        edge_index = torch.tensor(edges.T - 1, dtype=torch.long)
+        edge_attr = torch.tensor(mpc['branch'].iloc[:, 2:].to_numpy(), dtype=torch.float32)
 
-        edge_attr = mpc['branch'].iloc[:, 2:].to_numpy()
-        edge_attr = torch.tensor(edge_attr, dtype=torch.float32)
+        # NOTE: dummy y
+        data = Data(x=x,
+                    edge_index=edge_index,
+                    edge_attr=edge_attr,
+                    y=x[:, 11])
 
-        pos = mpc['bus_gmd'].to_numpy()
-        pos = torch.tensor(pos, dtype=torch.float32)
-
-        # REVIEW: data label or not (y)?
-        # hetero_graph:
-        # https://pytorch-geometric.readthedocs.io/en/latest/modules/data.html#torch_geometric.data.HeteroData
-        data = Data(x=x, edge_index=edge_index, edge_attr=None,)
-        # save to the processed path
         torch.save(self.collate([data]), self.processed_paths[0])
 
     def __repr__(self) -> str:
--- test_dataset.py
+++ test_dataset.py
@@ -1,10 +1,48 @@
 from py_script.dataset import GMD
 import torch_geometric.utils as pygutils
+from torch_geometric.nn import SAGEConv, GCNConv
+import torch
+from torch import nn
+# Simple GNN model
+
+
+class GNN(torch.nn.Module):
+
+    def __init__(self, in_channel=35, out_channel=1):
+        super(GNN, self).__init__()
+
+        self.in_channel = in_channel
+        self.out_channel = out_channel
+        self.conv1 = SAGEConv(in_channel, 128)
+        self.conv2 = SAGEConv(128, out_channel)
+        self.activation = torch.tanh
+
+    def forward(self, data):
+
+        x, edge_index = data.x, data.edge_index
+        x = self.conv1(x, edge_index)
+        # print(x.shape)
+        x = self.activation(x)
+        x = self.conv2(x, edge_index)
+        x = self.activation(x)
+
+        return x
+
 
 if __name__ == "__main__":
-    dataset = GMD("./test/data", name="epri21")
+    dataset = GMD("./test/data", name="b4gic")
     print(dataset)
     data = dataset[0]
-    net = pygutils.to_networkx(data)
-    print(f"# nodes {len(net.nodes)}",
-          f"# of edges {len(net.edges)}")
+    model = GNN(in_channel=data.num_node_features, out_channel=1)
+    loss_fn = nn.MSELoss()
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)  # Optimizer: Adam usually works well
+    losses = []
+    batch_size = 4  # Needs to be tuned
+    for i in range(100):
+        optimizer.zero_grad()
+        output = model(data)
+        loss = loss_fn(output, data.y)
+        loss.backward()
+        optimizer.step()
+        print(f"Epoch {i+1}, MSE loss {loss:.4f}")
