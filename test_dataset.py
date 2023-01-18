207434c4b351bb54487e84869539838d01cb57fd
H. Jin <jinh@anl.gov>
2023-01-18 12:30:51 -0600

    clean up code

--- test_dataset.py
+++ test_dataset.py
@@ -1,48 +1,108 @@
-from py_script.dataset import GMD
-import torch_geometric.utils as pygutils
-from torch_geometric.nn import SAGEConv, GCNConv
+""" Process dataset with HeteroData and a demo of HeterGNN network """
+
+import argparse
+
+import matplotlib.pyplot as plt
 import torch
+from scipy.special import softmax
 from torch import nn
-# Simple GNN model
+from torch_geometric.nn import HGTConv, Linear
+
+from py_script.dataset import GMD
 
 
-class GNN(torch.nn.Module):
+class HGT(torch.nn.Module):
+    def __init__(self, hidden_channels, out_channels, num_heads, num_layers):
+        super().__init__()
 
-    def __init__(self, in_channel=35, out_channel=1):
-        super(GNN, self).__init__()
+        self.lin_dict = torch.nn.ModuleDict()
+        for node_type in data.node_types:
+            self.lin_dict[node_type] = Linear(-1, hidden_channels)
 
-        self.in_channel = in_channel
-        self.out_channel = out_channel
-        self.conv1 = SAGEConv(in_channel, 128)
-        self.conv2 = SAGEConv(128, out_channel)
-        self.activation = torch.tanh
+        self.convs = torch.nn.ModuleList()
+        for _ in range(num_layers):
+            conv = HGTConv(hidden_channels, hidden_channels, data.metadata(),
+                           num_heads, group='sum')
+            self.convs.append(conv)
 
-    def forward(self, data):
+        self.lin = Linear(hidden_channels, out_channels)
 
-        x, edge_index = data.x, data.edge_index
-        x = self.conv1(x, edge_index)
-        # print(x.shape)
-        x = self.activation(x)
-        x = self.conv2(x, edge_index)
-        x = self.activation(x)
+    def forward(self, x_dict, edge_index_dict):
+        for node_type, x in x_dict.items():
+            x_dict[node_type] = self.lin_dict[node_type](x).relu_()
 
-        return x
+        for conv in self.convs:
+            x_dict = conv(x_dict, edge_index_dict)
+
+        return self.lin(x_dict['gmd_bus'])
 
 
 if __name__ == "__main__":
-    dataset = GMD("./test/data", name="b4gic")
-    print(dataset)
-    data = dataset[0]
-    model = GNN(in_channel=data.num_node_features, out_channel=1)
-    loss_fn = nn.MSELoss()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--name", type=str, default="b4gic",
+                        help="name of network")
+    parser.add_argument("--problem", "-p", type=str, default="clf", choices=["clf", "reg"],
+                        help="Specify the problem, either `clf` or `reg`")
+    parser.add_argument("--force", action="store_true",
+                        help="Force to reprocess data")
+    parser.add_argument("--lr", type=float, default=1e-3,
+                        help="learning rate")
+    parser.add_argument("--weight_decay", type=float, default=1e-4,
+                        help="weight decay rate for Adam")
+    parser.add_argument("--hidden_size", type=int, default=128,
+                        help="hidden dimension in HGT")
+    parser.add_argument("--num_heads", type=int, default=2,
+                        help="number of heads in HGT")
+    parser.add_argument("--num_layers", type=int, default=1,
+                        help="number of layers in HGT")
+    parser.add_argument("--epochs", type=int, default=200,
+                        help="number of epochs in training")
+    args = vars(parser.parse_args())
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)  # Optimizer: Adam usually works well
+    # readout the dataset `pyg.heterodata`
+    dataset = GMD("./test/data", name="epri21", problem=args['problem'], force_reprocess=args['force'])
+    data = dataset[0]
+
+    # adjust the output dimension accordingly
+    out_channels = 2 if args['problem'] == "clf" else 1
+    model = HGT(hidden_channels=args['hidden_size'],
+                out_channels=out_channels,
+                num_heads=args['num_heads'],
+                num_layers=args['num_layers'])
+
+    # adjust the loss function accordingly
+    loss_fn = nn.CrossEntropyLoss() if args['problem'] == "clf" else nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=args['lr'], weight_decay=args['weight_decay'])
+
     losses = []
-    batch_size = 4  # Needs to be tuned
-    for i in range(100):
+    model.train()
+    for epoch in range(args['epochs']):
         optimizer.zero_grad()
-        output = model(data)
-        loss = loss_fn(output, data.y)
+        # data.edge_attr_dict
+        # mini-batch settings for multi-graphs
+        out = model(data.x_dict, data.edge_index_dict)
+        if args['problem'] == "reg":
+            # REVIEW: meet the dimension of target
+            out = out.T[0]
+        loss = loss_fn(out, data['y'])
         loss.backward()
         optimizer.step()
-        print(f"Epoch {i+1}, MSE loss {loss:.4f}")
+        print(f"epoch {epoch}, loss {loss:.4f}")
+        losses.append(loss.item())
+    print("True bic_placed", data.y)
+    out = out.detach().cpu().numpy()
+    if args['problem'] == "clf":
+        print(softmax(out, axis=1))
+        print("Pred bic_placed", out.argmax(1))
+    else:
+        print(out)
+    # print("Predicted bic_placed", out.argmax(1))
+
+    ''' plot the loss function '''
+    fig = plt.figure(figsize=(4, 3), tight_layout=True)
+    foo = r'training loss'
+    plt.plot(losses)
+    plt.ylabel(foo)
+    plt.xlabel("epoch")
+    plt.title(f"Hete-Graph - {args['problem']}")
+    plt.savefig(f"losses - {args['problem']}.png")
--- test_read_file.py
+++
@@ -1,6 +0,0 @@
-from py_script.utils import read_file
-
-if __name__ == "__main__":
-    mpc = read_file("./test/data/epri21.m")
-    for k in mpc:
-        print(k, mpc[k])
