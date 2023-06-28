6530edab6606800af3b2ee317743842074f34a72
H. Jin <jinh@anl.gov>
2023-06-28 17:21:52 -0500

    fixed: issue with `hidden_size`

--- hps.py
+++ hps.py
@@ -1,61 +1,170 @@
+"""TODO:
+* test with multiple objectives, return accuracy and ROC-AUC scoore the validation set
+"""
+
 # %%
 import os.path as osp
 
+import matplotlib.pyplot as plt
 import numpy as np
-
+import pandas as pd
 import torch
 import torch.nn.functional as F
 import torch_geometric.transforms as T
 from deephyper.evaluator import Evaluator
 from deephyper.evaluator.callback import TqdmCallback
 from deephyper.problem import HpProblem
-from deephyper.search.hps import CBO, AMBS
+from deephyper.search.hps import CBO
+from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
 from torch_geometric.loader import DataLoader
 from tqdm import tqdm
-from sklearn.metrics import roc_auc_score
 
 from py_script.dataset import GMD, MultiGMD
 from py_script.model import HGT
 from py_script.transforms import NormalizeColumnFeatures
 
-import matplotlib.pyplot as plt
-
-import pandas as pd
-
 torch.manual_seed(12345)
+
 
 # %% [run]
 
-"""TODO:
-* fix the issue of the `run` function, return accuracy of validation set
-* update the GMDs with new class init
-* update the model with new class init
-* clean up the code
-* test with multiple objectives, return accuracy and ROC-AUC scoore the validation set
-"""
+def train(model, optimizer, loader, epochs=200, **kwargs):
+    r""" Training for the model.
+
+    Args:
+        model (object): Model to be trained.
+        optimizer (torch.optimizer): Optimizer for the model.
+        loader (pyg.DataLoader): Train DataLoader for the model.
+        epoch (int, optional): Number of epochs. Defaults to 200.
+
+    Returns:
+        tuple: (list of accuracies, list of rocs, list of losses)
+    """
+    _setting = kwargs.get("setting", "gic")
+    _verbose = kwargs.get("verbose", False)
+
+    # return a set of lists
+    all_acc, all_roc_auc, all_loss = [], [], []
+
+    if _verbose:
+        pbar = tqdm(range(epochs), desc="Training", leave=False)
+    else:
+        pbar = range(epochs)
+
+    model.train()
+    for _ in pbar:
+        t_loss = 0
+        all_true_labels, all_pred_labels = [], []
+        for i, data in enumerate(loader):
+
+            optimizer.zero_grad()
+
+            # Decide between MLD (reg) or GIC (cls)
+            if _setting == "mld":
+                out = model(data)[data.load_bus_mask]
+                loss = F.mse_loss(out, data['y'])
+            else:
+                out = model(data, "gmd_bus")
+
+                # Apply weighted cross entropy loss
+                if weight_arg and (len(data['y'].bincount()) > 1):
+                    weight = len(data['y']) / (2 * data['y'].bincount())
+                    loss = F.cross_entropy(out, data['y'], weight=weight)
+                else:
+                    loss = F.cross_entropy(out, data['y'])
+
+                y_true_batch = data['y'].detach().cpu().numpy()
+                y_pred_batch = out.argmax(dim=1).detach().cpu().numpy()
+                all_true_labels.extend(y_true_batch)
+                all_pred_labels.extend(y_pred_batch)
+
+            # update loss
+            t_loss += loss.item()
+            # update parameters
+            loss.backward()
+            optimizer.step()
+
+        all_true_labels = np.array(all_true_labels)
+        all_pred_labels = np.array(all_pred_labels)
+
+        train_acc = (all_true_labels == all_pred_labels).sum() / len(all_true_labels)
+        train_roc_auc = roc_auc_score(all_true_labels, all_pred_labels)
+
+        all_acc.append(train_acc)
+        all_roc_auc.append(train_roc_auc)
+        all_loss.append(t_loss)
+
+        if _verbose:
+            pbar.set_postfix({"loss": t_loss, "acc": train_acc, "roc_auc": train_roc_auc})
+
+    return all_acc, all_roc_auc, all_loss
+
+
+def evaluate(model, loader, **kwargs):
+    r""" Evaluate the model.
+
+    Args:
+        model (object): Model to be trained.
+        loader (pyg.DataLoader): DataLoader to evaluate the model.
+
+    Returns:
+        tuple: (accuracy, roc_auc, loss)
+    """
+    model.eval()
+    _setting = kwargs.get("setting", "gic")
+
+    t_loss = 0
+    all_true_labels, all_pred_labels = [], []
+    for i, data in enumerate(loader):
+
+        # Decide between MLD (reg) or GIC (cls)
+        if _setting == "mld":
+            out = model(data)[data.load_bus_mask]
+            loss = F.mse_loss(out, data['y'])
+        else:
+            out = model(data, "gmd_bus")
+
+            # Apply weighted cross entropy loss
+            if weight_arg and (len(data['y'].bincount()) > 1):
+                weight = len(data['y']) / (2 * data['y'].bincount())
+                loss = F.cross_entropy(out, data['y'], weight=weight)
+            else:
+                loss = F.cross_entropy(out, data['y'])
+
+            y_true_batch = data['y'].detach().cpu().numpy()
+            y_pred_batch = out.argmax(dim=1).detach().cpu().numpy()
+            all_true_labels.extend(y_true_batch)
+            all_pred_labels.extend(y_pred_batch)
+
+        # update loss
+        t_loss += loss.item()
+
+    all_true_labels = np.array(all_true_labels)
+    all_pred_labels = np.array(all_pred_labels)
+
+    acc = (all_true_labels == all_pred_labels).sum() / len(all_true_labels)
+    roc_auc = roc_auc_score(all_true_labels, all_pred_labels)
+
+    return acc, roc_auc, t_loss
 
 
 def run(config):
-    # NOTE: the run function is the function to __maximize__.
+    r""" The black-box function for HPS to maximize.
 
-    pre_transform = T.Compose([NormalizeColumnFeatures(["x", "edge_attr"])])
-    pre_transform = None
+    Args:
+        config (dict): The configuration of the hyperparameters.
 
-    setting = "gic"
-    weight_arg = True
-    # dataset = GMD(ROOT,
-    #               name="uiuc150",
-    #               setting=setting,
-    #               force_reprocess=False,
-    #               pre_transform=pre_transform)
-    dataset = MultiGMD(ROOT,
-                       names=["epri21", "uiuc150"],
-                       setting=setting,
-                       force_reprocess=False,
-                       pre_transform=pre_transform)
-    data = dataset[0]
+    Returns:
+        float: the objective value to be maximized.
 
+    References:
+        * [DeepHyper](https://deephyper.readthedocs.io/en/latest/tutorials/tutorials/colab/HPS_basic_classification_with_tabular_data/notebook.html#Define-the-run-function)
+
+    Notes:
+        * The run function is the black-box objective to __maximize__.
+    """
+    # HPS for model:
     batch_size = config.get("batch_size", 64)
     conv_type = config.get("conv_type", "hgt")
     hidden_channels = config.get("hidden_channels", 64)
@@ -64,12 +173,11 @@ def run(config):
     num_heads = config.get("num_heads", 2)
     num_layers = config.get("num_layers", 1)
     dropout = config.get("dropout", 0.5)
+    # HPS for optimizer:
     lr = config.get("lr", 1e-3)
     weight_decay = config.get("weight_decay", 1e-4)
 
     # NOTE: validate the hps settings
-
-    epochs = 200
 
     # Create a DataLoader for our datasets
     loader_train = DataLoader(dataset=dataset_train,
@@ -79,10 +187,6 @@ def run(config):
     loader_val = DataLoader(dataset=dataset_val,
                             batch_size=batch_size,
                             shuffle=True)
-
-    # loader_test = DataLoader(dataset=dataset_test,
-    #                          batch_size=batch_size,
-    #                          shuffle=True)
 
     model = HGT(hidden_channels=hidden_channels,
                 conv_type=conv_type,
@@ -96,115 +200,29 @@ def run(config):
                 metadata=data.metadata(),
                 ).to(DEVICE)
 
-    loss_fn = torch.nn.MSELoss()
-
     optimizer = torch.optim.Adam(model.parameters(),
                                  lr=lr,
                                  weight_decay=weight_decay)
 
-    losses = []
-    pbar = tqdm(range(epochs), desc="epri21")
-    model.train()
-    for epoch in pbar:
-        t_loss = 0
-        for i, data in enumerate(loader_train):
-            data = data.to(DEVICE)
-            optimizer.zero_grad()
+    train_accs, train_roc_aucs, train_losses = train(model, optimizer, loader_train, verbose=False)
 
-            # Decide between MLD or GIC
-            if setting == "mld":
-                out = model(data)[data.load_bus_mask]
-                loss = F.mse_loss(out, data['y'])
-            else:
-                out = model(data, "gmd_bus")
-                if weight_arg and (len(data['y'].bincount()) > 1):
-                    weight = len(data['y']) / (2 * data['y'].bincount())
-                    loss = F.cross_entropy(out, data['y'], weight=weight)
-                else:
-                    loss = F.cross_entropy(out, data['y'])
+    acc, roc_auc, loss = evaluate(model, loader_val)
 
-                train_acc = (data['y'].detach().cpu().numpy() == out.argmax(
-                    dim=1).detach().cpu().numpy()).sum() / len(data['y'])
-                # ROC_AUC score can't be computed when there's only one class (all zeroes in the true output)
-                if len(data['y'].bincount()) > 1:
-                    # There is at least one blocket placed, so we can compute the roc_auc score
-                    train_roc_auc = roc_auc_score(data['y'].detach().cpu().numpy(), out.argmax(1).detach().cpu().numpy())
-                else:
-                    # TODO: change the default value of roc_auc. We're supposed to throw an error, though.
-                    train_roc_auc = 0
+    return acc
 
-            loss.backward()
-            optimizer.step()
-
-            # FIXED: we don't need to devide by num_graphs
-            t_loss += loss.item()
-
-        # Choose how to handle the pbar based on the problem setting
-        if setting == "mld":
-            pbar.set_postfix({"loss": t_loss})
-        # else:
-        #     # pbar.set_postfix({"loss": t_loss, "train_acc": train_acc, "roc_auc": roc_auc})
-        losses.append(t_loss)
-
-    model.eval()
-    val_loss = 0
-    for batch in loader_val:
-        batch = batch.to(DEVICE)
-
-        # Compute between MLD or GIC
-        if setting == "mld":
-            pred = model(batch)[batch.load_bus_mask]
-            loss = F.mse_loss(pred, batch.y)
-        else:
-            # Use the mask to prune cases where there are no busses
-            pred = model(batch, "gmd_bus")
-            test_y = data['y']
-            # pred = model(batch, "gmd_bus")[batch.gic_blocker_bus_mask]
-            # test_y = data['y'][batch.gic_blocker_bus_mask]
-
-            if weight_arg and (len(batch.y.bincount()) > 1):
-                weight = len(batch.y) / (2 * (batch.y).bincount())
-                loss = F.cross_entropy(pred, batch.y, weight=weight)
-            else:
-                loss = F.cross_entropy(pred, batch.y)
-
-            # Some more objective functions
-            val_acc = (batch.y.detach().cpu().numpy() == pred.argmax(
-                1).detach().cpu().numpy()).sum() / len(batch.y)
-
-            # ROC_AUC score can't be computed when there's only one class (all zeroes in the true output)
-            if len(batch.y.bincount()) > 1:
-                # There is at least one blocket placed, so we can compute the roc_auc score
-                val_roc_auc = roc_auc_score(batch.y.detach().cpu().numpy(), pred.argmax(1).detach().cpu().numpy())
-            else:
-                # TODO: change the default value of roc_auc. We're supposed to throw an error, though.
-                val_roc_auc = 0
-
-        val_loss += loss.item() / batch.num_graphs
-
-    # maximize the value, so return the negative value of loss, or positive acc/roc-auc score.
-    # TOFIX: WRONG return value
-    # example:
-    # https://deephyper.readthedocs.io/en/latest/tutorials/tutorials/colab/HPS_basic_classification_with_tabular_data/notebook.html#Define-the-run-function
-    # return (-val_loss, val_acc, roc_auc)
-    # return (val_acc, val_roc_auc)
-    return val_acc
-
-    # MOO: (val_acc, val_roc_auc)
-    # SOO: -val_loss OR val_acc OR roc_auc
-
-    # return -test_loss
-    # return test_acc
-    # return roc_auc
+    # MOO: (acc, roc_auc)
+    # SOO: -loss OR acc OR roc_auc
 
 
 # %%
 ROOT = osp.join(osp.expanduser("~"), "tmp", "data", "GMD")
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# REVIEW: why we have to use CPU?
 DEVICE = torch.device('cpu')
 
 pre_transform = T.Compose([NormalizeColumnFeatures(["x", "edge_attr"])])
-pre_transform = None
+# # REVIEW: why we set None to pre_transform?
+# # pre_transform = None
 
 setting = "gic"
 weight_arg = True
@@ -213,78 +231,113 @@ dataset = GMD(ROOT,
               setting=setting,
               force_reprocess=False,
               pre_transform=pre_transform)
-# dataset = MultiGMD(ROOT,
-#                    names=["epri21", "uiuc150"],
-#                    setting=setting,
-#                    force_reprocess=True,
-#                    pre_transform=pre_transform)
+# # dataset = MultiGMD(ROOT,
+# #                    names=["epri21", "uiuc150"],
+# #                    setting=setting,
+# #                    force_reprocess=True,
+# #                    pre_transform=pre_transform)
+
 data = dataset[0]
 
-# %%
-# train/val/test: 0.8/0.1/0.1
+# # train/val/test: 0.8/0.1/0.1
 dataset_train, dataset_test = train_test_split(dataset, test_size=0.2, random_state=41)
 dataset_val, dataset_test = train_test_split(dataset_test, test_size=0.5, random_state=41)
 
-import torch
-base_cls = torch.nn.Module
-base_cls_repr = 'Act'
-acts = [
-    act for act in vars(torch.nn.modules.activation).values()
-    if isinstance(act, type) and issubclass(act, base_cls)
-]
 
 problem = HpProblem()
-# TODO: add hyperparameters
-# Note: hidden_channels, hidden_size, and batch_size orignally didn't have [1, 2, 4, 8].
-#       I added those only because DeepHyper will error out if 1 isn't one of the
-#       hyperparameter options.
-# problem.add_hyperparameter([16, 32, 64, 128, 256],
-#                            "hidden_size", default_value=128)
-# problem.add_hyperparameter([16, 32, 64, 128, 256],
-#                            "batch_size", default_value=64)
 
-# DEBUG: must include hidden_size=1
-problem.add_hyperparameter([1, 16, 32, 64, 128, 256],
+# HPS for model:
+problem.add_hyperparameter([16, 32, 64, 128, 256],
                            "hidden_size", default_value=128)
-problem.add_hyperparameter([1, 16, 32, 64, 128, 256],
+problem.add_hyperparameter([16, 32, 64, 128, 256],
                            "batch_size", default_value=64)
 problem.add_hyperparameter([1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
                            "num_conv_layers", default_value=1)
 problem.add_hyperparameter([1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
                            "num_mlp_layers", default_value=1)
 activation_choices = ["relu", "sigmoid", "tanh", "elu", "leakyrelu"]
-problem.add_hyperparameter(activation_choices, "activation", default_value="relu")
+problem.add_hyperparameter(activation_choices,
+                           "activation", default_value="relu")
 problem.add_hyperparameter(["hgt", "han"],
                            "conv_type", default_value="hgt")
 problem.add_hyperparameter([1, 2, 4, 8],
                            "num_heads", default_value=2)
 problem.add_hyperparameter([0., 0.1, 0.2, 0.3, 0.4, 0.5],
                            "dropout", default_value=0.5)
+# HPS for optimizer:
 problem.add_hyperparameter((1e-5, 1e-1, "log-uniform"),
                            "lr", default_value=1e-3)
 problem.add_hyperparameter([0., 1e-5, 5e-5, 1e-4],
                            "weight_decay", default_value=0.)
+# problem.add_hyperparameter((1.0, 3.0), "x")
+
+thread_kwargs = {"num_workers": 8,
+                 "callbacks": [TqdmCallback()]}
+ray_kwargs = {"num_cpus": 4,
+              "num_gpus": 1,
+              "num_cpus_per_task": 1,
+              "num_gpus_per_task": 1,
+              "callbacks": [TqdmCallback()]}
 
 evaluator = Evaluator.create(run,
                              method="thread",
-                              method_kwargs={
-                             #      "address": None,
-                             #      "num_gpus": 1,
-                             #      "num_gpus_per_task": 1,
-                             #      "num_cpus": 32,
-                             #      "num_cpus_per_task": 4,
-                             "num_workers": 32, 
-                                  "callbacks": [TqdmCallback()]
-                              }
+                             method_kwargs=thread_kwargs
                              )
+# evaluator = Evaluator.create(run,
+#                              method="ray",
+#                              method_kwargs=ray_kwargs
+#                              )
 print("Number of workers: ", evaluator.num_workers)
 
-# %%
-search = CBO(problem, evaluator, initial_points=[problem.default_configuration])
+search = CBO(problem, evaluator, initial_points=[problem.default_configuration], random_state=42)
 # Print all the results
 print("All results:")
-results = search.search(max_evals=100)
+results = search.search(max_evals=200, )
 print(results)
+results.to_csv("results.csv")
+
+i_max = results['objective'].argmax()
+# DEBUG:
+best_hp = results.iloc[i_max][0:-3].to_dict()
+print("Best HPS: ", best_hp)
+
+# retrain the model with best HPS setting
+best_model = HGT(hidden_channels=best_hp['p:hidden_size'],
+                 conv_type=best_hp['p:conv_type'],
+                 num_mlp_layers=best_hp['p:num_mlp_layers'],
+                 activation=best_hp['p:activation'],
+                 out_channels=2,
+                 num_heads=best_hp['p:num_heads'],
+                 num_conv_layers=best_hp['p:num_conv_layers'],
+                 dropout=best_hp['p:dropout'],
+                 node_types=data.node_types,
+                 metadata=data.metadata(),
+                 ).to(DEVICE)
+best_optimizer = torch.optim.Adam(best_model.parameters(), lr=best_hp['p:lr'], weight_decay=best_hp['p:weight_decay'])
+
+# Create a DataLoader for our datasets
+loader_train = DataLoader(dataset=dataset_train,
+                          batch_size=best_hp['p:batch_size'],
+                          shuffle=True)
+
+loader_val = DataLoader(dataset=dataset_val,
+                        batch_size=best_hp['p:batch_size'],
+                        shuffle=True)
+
+loader_test = DataLoader(dataset=dataset_test,
+                         batch_size=best_hp['p:batch_size'],
+                         shuffle=True)
+
+
+train_metrics = train(best_model, best_optimizer, loader_train)
+test_metrics = evaluate(best_model, dataset_test)
+print(f"Test accuracy: {test_metrics[0]:.4f}",
+      f"Test ROC-AUC {test_metrics[1]:.4f}")
+
+# TODO:
+# plot the radar chart for the best HPS setting from `test_metrics`
+# plot the training curve for the best HPS setting from `train_metrics`
+
 
 # 1. get the best HPS setting from validation set
 # 2. train the model again with best HPS setting
@@ -358,7 +411,7 @@ print(results)
 #         label.set_horizontalalignment('right')
 
 # Save the results to a CSV file
-results.to_csv("results.csv")
+
 
 # https://plotly.com/python/radar-chart/
 # https://www.pythoncharts.com/matplotlib/radar-charts/
