d81579e23cc8fc8ff7aad5c9e5bd4347cd84b53a
H. Jin <jinh@anl.gov>
2023-08-19 19:27:22 -0500

    new `data` folder for gic heuristic solver

---
+++ data/install.jl
@@ -0,0 +1,11 @@
+import Pkg
+
+Pkg.add("ArgParse")
+Pkg.add("DataFrames")
+Pkg.add("Ipopt")
+Pkg.add("JSON")
+Pkg.add("JuMP")
+Pkg.add("Juniper")
+Pkg.add("MathOptInterface")
+Pkg.add("SCIP")
+Pkg.add("XLSX")
